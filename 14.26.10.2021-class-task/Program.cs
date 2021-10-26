89dde1bac10ba5fcce7af435574e648c4d5a68bd
Hajiyev <tu201806256@aztu.local>
2021-10-26 09:23:34 +0400

    initial commit

---
+++ 14.26.10.2021-class-task/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _14._26._10._2021_class_task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
